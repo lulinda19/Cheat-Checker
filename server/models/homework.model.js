112a13da9c4d36ef9b5ef5e426c6a764c2802677
Linda Lu <Linda@Lindas-MacBook-Pro.local>
2020-04-04 17:01:14 -0400

    fixed bugs in server

--- server/models/homework.model.js
+++ server/models/homework.model.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const mongoose = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const homeworkSchema = new Schema({
   // Homework name is used as a unique identifier, is required
-  name: { type: String, required: true, unique: true, minlength = 1 },
+  name: { type: String, required: true, unique: true, minlength: 1 },
   startMonitoringDate: { type: Date, required: true },
   endMonitoringDate: { type: Date, required: true },
   keywords: [{ type: String, trim: true }],
